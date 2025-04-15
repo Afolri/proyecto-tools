c7f624ea2ed847c13722fe2d9a01878e17fca1ce
Afolri <arizzo@sistemasescom.com>
2025-04-15 17:32:06 -0600

    eliminación de socket management

--- multitools/src/environments/environment.ts
+++ multitools/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     URL_BASE: 'http://localhost:8080',
-    URL_BASE_SOCKET:'http://localhost:8080/admin/socket'
+    URL_BASE_SOCKET:'ws://localhost:8080'
 }
