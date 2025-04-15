2faffd326072cda8840b0f9b2034e462e791694b
Afolri <arizzo@sistemasescom.com>
2025-04-15 17:15:47 -0600

    cambio personal corregido dudoso 2

--- multitools/src/environments/environment.prod.ts
+++ multitools/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
         production: true,
         URL_BASE: '/api',
-        URL_BASE_SOCKET:'wss://www.emultitools.com/admin/socket'
+        URL_BASE_SOCKET:'wss://www.emultitools.com/admin/pan'
 };
