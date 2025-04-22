d214a3fea8c045d9ca0c6eb609d554755b04a9ff
Afolri <arizzo@sistemasescom.com>
2025-04-21 18:03:29 -0600

    cambio de ip

--- multitools/src/environments/environment.prod.ts
+++ multitools/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
         production: true,
         URL_BASE: '/api',
-        URL_BASE_SOCKET:'https://www.emultitools.local'
+        /**URL_BASE_SOCKET:'https://www.emultitools.local'*/
+        URL_BASE_SOCKET:'https://192.168.2.96'
 };
